df1cdc73d172951b10746f66d82ab19dfb3bef97
Wajeeh Mahmood <wajeeh@Wajeehs-MacBook-Pro-2.local>
2026-01-31 23:45:40 +0000

    upload 2

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import "./App.css";
+import ConditionWheel from "./condition-wheel.jsx";
+
+export default function App() {
+  return (
+    <div style={{ padding: 24 }}>
+      <ConditionWheel />
+    </div>
+  );
+}
+
